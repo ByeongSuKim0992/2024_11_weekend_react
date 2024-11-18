f5cd7d5b6b69e0240127ae78954a8c3727795158
ByeongSuKim0992 <byeongsu0707@gmail.com>
2024-11-18 20:41:06 +0900

    첫리액트

--- Desktop/Frontend/React/react_theory/src/App.jsx
+++ Desktop/Frontend/React/react_theory/src/App.jsx
@@ -1,24 +1,5 @@
-import BlueButton from "./BlueButton";
-import DeepBlueButton from "./DeepBlueButton";
-import HeartButton from "./HeartButton";
-import Cart from "./Cart";
-import LikeButton from "./LikeButton";
-import SubscribeButton from "./SubscribeButton";
-
-// 구독 버튼 컴포넌트
-// 하트 💛 버튼 컴포넌트
-
 function App() {
-  return (
-    <div className="App">
-      {/* <LikeButton></LikeButton> */}
-      {/* <SubscribeButton></SubscribeButton> */}
-      <HeartButton></HeartButton>
-      <BlueButton></BlueButton>
-      <DeepBlueButton></DeepBlueButton>
-      <Cart></Cart>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
