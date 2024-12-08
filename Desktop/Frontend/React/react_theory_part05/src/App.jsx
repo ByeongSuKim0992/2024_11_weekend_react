d884bee928c29c2e1c3ab87b5fdcb238ffc3a6d3
ByeongSuKim0992 <byeongsu0707@gmail.com>
2024-12-08 21:42:55 +0900

    react_theory_part05

--- Desktop/Frontend/React/react_theory_part05/src/App.jsx
+++ Desktop/Frontend/React/react_theory_part05/src/App.jsx
@@ -2,8 +2,8 @@
 // import CharNum from "./compenents/presentation/CharNum";
 // import Input from "./compenents/presentation/Input";
 
-import { useEffect, useState } from "react";
-import Book from "./compenents/presentation/Book";
+// import { useState } from "react";
+// import Company from "./compenents/presentation/Company";
 
 // function App() {
 //   const [num, setNum] = useState("");
@@ -19,23 +19,76 @@ import Book from "./compenents/presentation/Book";
 //   );
 // }
 
-function App() {
-  const [books, setBooks] = useState();
+// import { useEffect, useState } from "react";
+// import Book from "./compenents/presentation/Book";
 
-  // 생명주기[태어나고 - 살고 - 죽고]
-  // Book[만들어지기 전 - 화면에 있을 때 - 삭제 될 때]
+// function App() {
+//   const [books, setBooks] = useState([]);
+
+// Web API[console, dom, storage, fetch, setinteral, settimeout]
+
+// 생명주기[태어나고 - 살고 - 죽고]
+// Book[만들어지기 전 - 화면에 있을 때 - 삭제 될 때]
+//   useEffect(() => {
+//     fetch("https://fakerapi.it/api/v2/books?_quantity=10")
+//       .then((response) => response.json())
+//       .then((v) => setBooks((prev) => [...v.data]));
+//   }, []);
+
+//   return (
+//     <div>
+//       {books.map((v) => (
+//         <Book {...v} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function app() {
+//   const [books, setBooks] = useState([]);
+//   const [companies, setCompanies] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://fakerapi.it/api/v2/books?_quantity=10")
+//       .then((response) => response.json())
+//       .then((v) => setBooks((prev) => [...v.data]));
+
+//     fetch("https://fakerapi.it/api/v2/companies?_quantity=10")
+//       .then((response) => response.json())
+//       .then((v) => setCompanies((prev) => [...v.data]));
+//   }, []);
+
+//   return (
+//     <div>
+//       {books.map((v) => (
+//         <Book {...v} />
+//       ))}
+//       {companies.map((v) => (
+//         <Company {...v} />
+//       ))}
+//     </div>
+//   );
+// }
+
+import { useEffect, useState } from "react";
+import Coffee from "./components/presentation/Coffee";
+import Menu from "./components/layout/Menu";
+
+function App() {
+  const [coffees, setCoffees] = useState([]);
+
   useEffect(() => {
-    fetch("https://fakerapi.it/api/v2/books?_quantity=10")
+    fetch("https://api.sampleapis.com/coffee/hot")
       .then((response) => response.json())
-      .then((v) => setBooks((prev) => [...v.data]));
+      .then((v) => setCoffees((prev) => [...v]));
   }, []);
 
   return (
-    <div>
-      {books.map((v) => (
-        <Book {...v} />
+    <Menu
+      children={coffees.map((v) => (
+        <Coffee {...v} />
       ))}
-    </div>
+    />
   );
 }
 
